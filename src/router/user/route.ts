65fcac35319a3ede38a1a73e6884d648fb1c8f3e
Biswajit Das <workbiswajit0981@gmail.com>
2025-08-25 12:09:58 +0530

    Enhance user routes and add authentication middleware.
    Added new protected routes for user profile, role change, and logout. Introduced `cookie-parser` dependency and defined `DecodedToken` type for better token handling. Updated package files to reflect these changes.

--- src/router/user/route.ts
+++ src/router/user/route.ts
@@ -1,12 +1,20 @@
 import express, {Router} from "express";
 import {userLogin} from "../../controller/user/login";
 import {userRegistration} from "../../controller/user/signup";
+import {Authentication} from "../../middleware/userMiddlewares/Authentication";
+import {userProfile} from "../../controller/user/userProfile";
+import {changeUserRole} from "../../controller/user/changeRole";
+import {userLogout} from "../../controller/user/logout";
 
 const userRouter = Router();
 
-//public routes
+
 
 userRouter.post("/api/user/login", userLogin);
 userRouter.post("/api/user/signup", userRegistration);
+
+userRouter.get("/api/user-profile", Authentication, userProfile);
+userRouter.patch("/api/user/change-role", Authentication, changeUserRole)
+userRouter.post("/api/user/logout", Authentication, userLogout);
 
 export default userRouter;

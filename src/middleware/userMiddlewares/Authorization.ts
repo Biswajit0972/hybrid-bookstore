754a0a726be239036d1ec83bd50a19a3077b05ff
Biswajit Das <workbiswajit0981@gmail.com>
2025-08-25 12:08:38 +0530

    Add Authorization middleware for role-based access control 🌍🌍🌍

---
+++ src/middleware/userMiddlewares/Authorization.ts
@@ -0,0 +1,11 @@
+import {NextFunction, Request, Response} from "express";
+
+export const Authorization = (role: string) => {
+    return async (req: Request, res: Response, next: NextFunction) => {
+        if (req.headers.role !== role) {
+            res.status(403).json({flag: false, message: "user is not authorized"});
+        }
+
+        next();
+    }
+}

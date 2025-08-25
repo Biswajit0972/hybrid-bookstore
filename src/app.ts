d9f7488dd8fe16f2e39800df9a0e3335b07bf388
Biswajit Das <workbiswajit0981@gmail.com>
2025-08-25 12:08:21 +0530

    Add cookie-parser middleware to app initialization 🌍🌍🌍

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import cookieParser from "cookie-parser";
 import superAdminRouter from "./router/superAdminRouter/router";
 import userRouter from "./router/user/route";
 
 const app = express();
 
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended: true, limit: "16kb"}));
 app.use(superAdminRouter);
